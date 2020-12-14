11b93e6cbb2aa02cb92cb62828f428dc5320aca9
unknown <31515@ma-web.nl>
2020-12-14 10:43:41 +0100

    Eerste WebXR test

--- Code/WebXR/app.js
+++ Code/WebXR/app.js
@@ -3,35 +3,24 @@ const controls = new Controls(50);
 controls.enableMovement();
 
 // create a model
-const minecraftModel = new Model('assets/models/minecraft/scene.gltf');
-minecraftModel.setPosition(3.04114, 1.54, -9.55352);
-minecraftModel.setScale(1.34, 0.1, 0.1);
-minecraftModel.setRotation(0, -36.95, 0);
-
-//create model cube model
-const cuberoom = new Model('assets/models/cube-room/cube-room.gltf');
-cuberoom.setPosition(-2.5, 0.91, -3.2);
-cuberoom.setScale(0.3, 0.4, 0.16);
-cuberoom.setRotation(0, -36.95, 0);
-// create a primitive (sphere)
-const sphere = new Sphere();
-sphere.setColor('green');
-sphere.setRadius(0.5);
-sphere.setPosition(-3, 0, -5);
+const laddermodel = new Model('Models/ladder.gltf');
+laddermodel.setPosition(3.04114, 1.54, -9.55352);
+laddermodel.setScale(1.34, 0.1, 0.1);
+laddermodel.setRotation(0, -36.95, 0);
 
 // create some text
-const testText = new Text('Javascript les 4');
+const testText = new Text('Test WebXR');
 testText.setPosition(1.5, 0, -5);
 testText.setFontsize(30);
 testText.setColor('green')
 
 
 // create a 360 image
-const image360 = new Sky('assets/images/FolkertdeJong.jpg');
+const image360 = new Sky('Textures/skybox_Test.jpg');
 
 const spotLight = new SpotLight();
 spotLight.setPosition(-3.39382, 4.72931, 1.37905);
-spotLight.setTarget(minecraftModel);
+spotLight.setTarget(laddermodel);
 
 // listen for an event
-minecraftModel.addEventListener('click', () => console.log('je klikt op mij'));
+laddermodel.addEventListener('click', () => console.log('je klikt op mij'));
