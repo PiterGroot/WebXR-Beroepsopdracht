2f2ffa64c0b2bf9bbe4cbc042b5a4f1672929c9a
unknown <31515@ma-web.nl>
2021-01-14 19:29:11 +0100

    Twee kunstwerken toegevoegd aan de WebXr scene

--- Code/Page/app.js
+++ Code/Page/app.js
@@ -1,5 +1,4 @@
 
-
 // voeg controls toe (teleport & WASD controls)
 const controls = new Controls(1000);
 controls.enableMovement();
@@ -8,21 +7,31 @@ controls.enableMovement();
 const roommodel = new Model('Models/room/scene.gltf')
 roommodel.setPosition(0, -2.332, 0);
 
+// create an image
+const image = new XRImage('Textures/table.jpg');
+image.setPosition(-1.48819, 2.45839, -7.621);
+image.setScale(3, 3, 3);
+// create an image
+const image1 = new XRImage('Textures/circle.jpg');
+image1.setPosition(2, 2.45839, -7.621);
+image1.setScale(3, 3, 3);
+
+
 //ladder model
 const laddermodel = new Model('Models/ladder/scene.gltf');
-laddermodel.setPosition(3.092, 0, -4.073);
-laddermodel.setScale(0.015, 0.015, 0.015);
+laddermodel.setPosition(15.535, -0.434, -7.295);
+laddermodel.setScale(0.025, 0.025, 0.025);
 laddermodel.setRotation(0, 0, 49.029);
 //workbench model
 const workbenchmodel = new Model('Models/bench/scene.gltf')
-workbenchmodel.setPosition(-3.495, 0, -2.008);
-workbenchmodel.setScale(0.01, 0.01, 0.01)
-workbenchmodel.setRotation(0, -80.789, 0)
+workbenchmodel.setPosition(-12.896, -0.903, 0.062);
+workbenchmodel.setScale(0.03, 0.03, 0.03)
+workbenchmodel.setRotation(0, -13.05, 0)
 
 //bucket model
 const bucketmodel = new Model('Models/bucket/scene.gltf');
-bucketmodel.setPosition(-1.82, 0.754, -4.355);
-bucketmodel.setScale(0.100, 0.100, 0.100);
+bucketmodel.setPosition(-11.976, 0.754, -4.365);
+bucketmodel.setScale(0.10, 0.10, 0.10);
 // create some text
 const testText = new Text('WebXR atelier');
 testText.setPosition(-23.250, 19.523, -5);
@@ -38,7 +47,12 @@ const image360 = new Sky('Textures/skybox.jpg');
 //spotLight.setIntensity(0.64);
 
 // listen for an event
-laddermodel.addEventListener('click', () => document.getElementById('extraText').innerHTML = "Ladder");
+image.addEventListener('mouseenter', () => document.getElementById('extraText').innerHTML = "Folkert de Jong Nihilist Table");
+image1.addEventListener('mouseenter', () => document.getElementById('extraText').innerHTML = "Folker de Jong Cirkel van het leven");
+image.addEventListener('mouseleave', () => document.getElementById('extraText').innerHTML = "");
+image1.addEventListener('mouseleave', () => document.getElementById('extraText').innerHTML = "");
+laddermodel.addEventListener('mouseenter', () => document.getElementById('extraText').innerHTML = "Ladder");
+laddermodel.addEventListener('mouseleave', () => document.getElementById('extraText').innerHTML = "");
 bucketmodel.addEventListener('mouseenter', () => document.getElementById('extraText').innerHTML= "Emmer");
 bucketmodel.addEventListener('mouseleave', () => document.getElementById('extraText').innerHTML= "...");
 workbenchmodel.addEventListener('mouseenter', () => document.getElementById('extraText').innerHTML= "Werktafel");
